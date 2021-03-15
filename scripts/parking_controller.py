00c91d0598d6aa99ca93aa7c88905838cbbd9925
inasarhaye <mmdmubaarik@gmail.com>
2021-03-15 12:53:59 -0400

    update parking controller

--- scripts/parking_controller.py
+++ scripts/parking_controller.py
@@ -1,57 +1,22 @@
 #!/usr/bin/env python
 import rospy
-from lab4.msg import cone_location, parking_error
+from geometry_msgs.msg import PointStamped
 from ackermann_msgs.msg import AckermannDriveStamped
 import numpy as np
 
 class ParkingController():
-    """
-    A controller for parking in front of a cone.
-    Listens for a relative cone location and publishes control commands.
-    Can be used in the simulator and on the real robot.
-    """
+
+    DRIVE_TOPIC = "/tesse/drive"
+    CONE_LOCATION = "/cone_relative"
+    CONE_DESIRED_HEIGHT = 125
+    CONE_DESIRED_ANGEL = 0.0
     def __init__(self):
-        rospy.Subscriber("/relative_cone", cone_location, 
-            self.relative_cone_callback)    
-        self.drive_pub = rospy.Publisher("/drive", 
-            AckermannDriveStamped, queue_size=10)
-        self.error_pub = rospy.Publisher("/parking_error",
-            parking_error, queue_size=10)
+        self.pub = rospy.Publisher(self.DRIVE_TOPIC, AckermannDriveStamped, queue_size = 5)
+        self.sub = rospy.Subscriber(self.CONE_LOCATION, PointStamped, self.driveCallback)
 
-        self.relative_x = 0
-        self.relative_y = 0
-
-    def relative_cone_callback(self, msg):
-        self.relative_x = msg.x_pos
-        self.relative_y = msg.y_pos
-        drive_cmd = AckermannDriveStamped()
-        
-        #################################
-        # Play with this number too
-        parking_distance = .75 #meters
-        
-        # Your Code Here.
-        # Use relative position and your control law to populate
-        # drive_cmd.
-        
-        #################################
-        self.drive_pub.publish(drive_cmd)
-        self.error_publisher()
-        
-    def error_publisher(self):
-        """
-        Publish the error between the car and the cone. We will view this
-        with rqt_plot to plot the success of the controller
-        """
-        error_msg = parking_error()
-        
-        #################################
-        
-        # Your Code Here
-        # Populate error_msg with relative_x, relative_y, sqrt(x^2+y^2)
-
-        #################################
-        self.error_pub.publish(error_msg)
+    def driveCallback(self,msg):
+        ##YOUR CODE
+        pass
 
 if __name__ == '__main__':
     try:
